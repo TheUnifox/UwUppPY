05c5649e7784451b06319cbf78eaf9a0b79b3335
Goldngamr5 <awsomejojop@gmail.com>
2022-09-11 16:29:40 -0700

    Fix Path

--- buildAndInstall.ps1
+++ buildAndInstall.ps1
@@ -4,5 +4,5 @@ pyinstaller -p / --onefile -n UwUpp main.py
 
 $currentDir = Get-Location
 $oldpath = (Get-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH).path
-$newpath = “$oldpath;$currentDir/dist;"
+$newpath = "$oldpath;$currentDir\dist;"
 Set-ItemProperty -Path 'Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment' -Name PATH -Value $newPath
